0d2fda8415e2f954de9b3bbe4a17b55ab2bb846c
Bae Sujeong <doya60@gmail.com>
2022-06-09 17:06:20 +0900

    유튜브 팝업 작업완료

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { useEffect } from "react";
 
-function Popup() {
-  return <div>Popup</div>;
+function Popup({ children, setOpen }) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  });
+
+  return (
+    <aside className="pop">
+      <div className="cont">
+        {children}
+        <span className="btnClose" onClick={() => setOpen(false)}>
+          close
+        </span>
+      </div>
+    </aside>
+  );
 }
 
 export default Popup;
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -2,9 +2,17 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Layout from "../common/Layout";
+import Popup from "../common/Popup";
 
 function Youtube() {
   const [Vids, setVids] = useState([]);
+  const [Open, setOpen] = useState(false);
+  const [Index, setIndex] = useState(0);
+
+  const handlePopup = (idx) => {
+    setOpen(true);
+    setIndex(idx);
+  };
 
   const fetchYoutube = () => {
     const key = "AIzaSyCNEFP7grGD77zUQvYF6Tg93dOjeA-mCjs";
@@ -20,33 +28,44 @@ function Youtube() {
   useEffect(fetchYoutube, []);
 
   return (
-    <Layout name={"Youtube"}>
-      <h1>YOUTUBE</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-        corrupti optio rerum. Sapiente ut id modi harum laudantium
-      </p>
-      <div className="wrap">
-        {Vids.map((vid, idx) => {
-          const tit = vid.snippet.title;
-          const desc = vid.snippet.description;
-          const date = vid.snippet.publishedAt;
+    <>
+      <Layout name={"Youtube"}>
+        <h1>YOUTUBE</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          corrupti optio rerum. Sapiente ut id modi harum laudantium
+        </p>
+        <div className="wrap">
+          {Vids.map((vid, idx) => {
+            const tit = vid.snippet.title;
+            const desc = vid.snippet.description;
+            const date = vid.snippet.publishedAt;
 
-          return (
-            <article key={idx}>
-              <span>{date.split("T")[0]}</span>
-              <div className="pic">
-                <img src={vid.snippet.thumbnails.high.url} alt={vid.title} />
-              </div>
-              <div className="txt-wrap">
-                <h2>{tit.length > 25 ? tit.substr(0, 25) + "..." : tit}</h2>
-                <p>{desc.length > 80 ? desc.substr(0, 80) + "..." : desc}</p>
-              </div>
-            </article>
-          );
-        })}
-      </div>
-    </Layout>
+            return (
+              <article key={idx}>
+                <span>{date.split("T")[0]}</span>
+                <div className="pic" onClick={() => handlePopup(idx)}>
+                  <img src={vid.snippet.thumbnails.high.url} alt={vid.title} />
+                </div>
+                <div className="txt-wrap">
+                  <h2>{tit.length > 25 ? tit.substr(0, 25) + "..." : tit}</h2>
+                  <p>{desc.length > 80 ? desc.substr(0, 80) + "..." : desc}</p>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </Layout>
+
+      {Open && (
+        <Popup setOpen={setOpen}>
+          <iframe
+            src={`https://www.youtube.com/embed/${Vids[Index].snippet.resourceId.videoId}`}
+            frameborder="0"
+          ></iframe>
+        </Popup>
+      )}
+    </>
   );
 }
 
