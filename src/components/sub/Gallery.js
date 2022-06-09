c62f9a0362c4afa8750379de401ed15bce4cb211
Bae Sujeong <doya60@gmail.com>
2022-06-09 15:02:49 +0900

    갤러리 레이아웃 작업

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,7 +4,20 @@ import Layout from "../common/Layout";
 function Gallery() {
   return (
     <Layout name={"Gallery"}>
-      <p>Gallery 페이지</p>
+      <div className="left">
+        <div className="title-wrap">
+          <h1>ROOM</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+            quo!
+          </p>
+        </div>
+      </div>
+      <div className="right">
+        <div className="title-wrap">
+          <h1>HOTEL</h1>
+        </div>
+      </div>
     </Layout>
   );
 }
