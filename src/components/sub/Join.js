7756e046b820c6fa812b737392fafeb7226e39cb
Bae Sujeong <doya60@gmail.com>
2022-06-10 00:22:56 +0900

    회원가입 작업

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,10 +1,274 @@
-import React from "react";
 import Layout from "../common/Layout";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function Join() {
+  const history = useHistory();
+
+  const initVal = {
+    userid: "",
+    pwd1: "",
+    pwd2: "",
+    email: "",
+    comments: "",
+    area: "",
+    genter: null,
+  };
+
+  const [Val, setVal] = useState(initVal);
+  const [Err, setErr] = useState({});
+  const [Success, setSuccess] = useState(false);
+  const [Submit, setSubmit] = useState(false);
+
+  const check = (Val) => {
+    const errs = {};
+    const eng = /[a-zA-Z]/;
+    const num = /[0-9]/;
+    const spc = /[~!@#$%^&*()_+]/;
+
+    //userid인증처리
+    if (Val.userid.length < 5) {
+      errs.userid = "아이디를 5글자 이상 입력하세요";
+    }
+
+    //password인증처리
+    if (
+      Val.pwd1.length < 5 ||
+      !eng.test(Val.pwd1) ||
+      !num.test(Val.pwd1) ||
+      !spc.test(Val.pwd1)
+    ) {
+      errs.pwd1 =
+        "비밀번호는 5글자 이상, 영문, 숫자, 특수문자를 모두 포함하세요";
+    }
+    if (Val.pwd1 !== Val.pwd2 || !Val.pwd2) {
+      errs.pwd2 = "두개의 비밀번호를 동일하게 입력하세요";
+    }
+
+    //email인증처리
+    if (Val.email.length < 8 || !/@/.test(Val.email)) {
+      errs.email = "이메일은 8글자이상 @를 포함해 입력하세요";
+    }
+
+    if (!Val.gender) {
+      errs.gender = "성별을 선택하세요";
+    }
+
+    if (Val.comments.length < 20) {
+      errs.comments = "남기는 말은 10글자 이상 입력해주세요";
+    }
+
+    if (Val.area === "") {
+      errs.area = "지역을 선택하세요";
+    }
+
+    return errs;
+  };
+
+  const handleReset = () => {
+    setSubmit(false);
+    setErr({});
+    setVal(initVal);
+  };
+
+  const handleChange = (e) => {
+    //2)입력하고 있는 input요소의 name, value값을 변수로 비구조화 할당
+    const { name, value } = e.target;
+    // console.log(`name:${name} / value: ${value}`);
+    //3)비구조화 할당으로 받은 값을 Val스테이트에 저장
+    // 이때 객체 리터럴 프로퍼티안에서 name값을 변수로 유지시키기 위해 []감싸줌
+    setVal({ ...Val, [name]: value });
+  };
+
+  const handleRadio = (e) => {
+    const { name } = e.target;
+    const isCheck = e.target.checked;
+    setVal({ ...Val, [name]: isCheck });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErr(check(Val));
+  };
+
+  const handleSelect = (e) => {
+    const { name } = e.target;
+    const isSelected = e.target.options[e.target.selectedIndex].value;
+    setVal({ ...Val, [name]: isSelected });
+  };
+
+  useEffect(() => {
+    const len = Object.keys(Err).length;
+    if (len === 0 && Submit) {
+      setSuccess(true);
+      history.push("/");
+    } else {
+      setSuccess(false);
+    }
+  }, [Err]);
+
   return (
     <Layout name={"Join"}>
-      <p>Join 페이지</p>
+      <h1>JOIN US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        aliquam voluptatem odio consectetur non atque pariatur, placeat nemo
+        reiciendis maxime.
+      </p>
+
+      <span>회원정보</span>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend className="hidden">회원가입 폼 양식</legend>
+          <table>
+            <caption className="hidden">회원가입 정보입력</caption>
+            <tbody>
+              {/* userid */}
+              <tr>
+                <th scope="col">
+                  <label htmlFor="userid">USER ID</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="userid"
+                    name="userid"
+                    placeholder="아이디를 입력해주세요."
+                    //4)Val값이 변경될때마다 화면에 출력
+                    value={Val.userid}
+                    //1)인풋에 값 입력시
+                    onChange={handleChange}
+                  />
+                  <span className="err">{Err.userid}</span>
+                </td>
+              </tr>
+
+              {/* password */}
+              <tr>
+                <th scope="col">
+                  <label htmlFor="pwd1">PASSWORD</label>
+                </th>
+                <td>
+                  <input
+                    type="password"
+                    name="pwd1"
+                    id="pwd1"
+                    placeholder="비밀번호를 입력해주세요."
+                    value={Val.pwd1}
+                    onChange={handleChange}
+                  />
+                  <span className="err">{Err.pwd1}</span>
+                </td>
+              </tr>
+              <tr>
+                <th scope="col">
+                  <label htmlFor="pwd2">PASSWORD</label>
+                </th>
+                <td>
+                  <input
+                    type="password"
+                    name="pwd2"
+                    id="pwd2"
+                    placeholder="비밀번호를 동일하게 재입력해주세요."
+                    value={Val.pwd2}
+                    onChange={handleChange}
+                  />
+                  <span className="err">{Err.pwd2}</span>
+                </td>
+              </tr>
+
+              {/* email */}
+              <tr>
+                <th scope="col">
+                  <label htmlFor="email">E-MAIL</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="이메일주소를 입력해주세요."
+                    value={Val.email}
+                    onChange={handleChange}
+                  />
+                  <span className="err">{Err.email}</span>
+                </td>
+              </tr>
+
+              {/* area */}
+              <tr>
+                <th scope="col">
+                  <label htmlFor="area">AREA</label>
+                </th>
+                <td>
+                  <select name="area" id="area" onChange={handleSelect}>
+                    <option value="">지역을 선택해주세요</option>
+                    <option value="seoul">서울특별시</option>
+                    <option value="gyeonggi">경기도</option>
+                    <option value="busan">부산광역시</option>
+                    <option value="jeju">제주도</option>
+                  </select>
+                  <span className="err">{Err.area}</span>
+                </td>
+              </tr>
+
+              {/* gender */}
+              <tr>
+                <th scope="col">GENDER</th>
+                <td>
+                  <label htmlFor="male">Male</label>
+                  <input
+                    type="radio"
+                    id="male"
+                    name="gender"
+                    onChange={handleRadio}
+                  />
+
+                  <label htmlFor="female">Female</label>
+                  <input
+                    type="radio"
+                    id="female"
+                    name="gender"
+                    onChange={handleRadio}
+                  />
+                  <span className="err">{Err.gender}</span>
+                </td>
+              </tr>
+
+              {/* comments */}
+              <tr className="w100">
+                <th scope="col">
+                  <label htmlFor="comments">COMMENTS</label>
+                </th>
+                <td>
+                  <textarea
+                    name="comments"
+                    id="comments"
+                    cols="30"
+                    rows="10"
+                    value={Val.comments}
+                    onChange={handleChange}
+                  ></textarea>
+                  <span className="err">{Err.comments}</span>
+                </td>
+              </tr>
+
+              {/* btnSet */}
+              <tr className="w100">
+                <th colSpan="2">
+                  <input type="reset" value="CANCEL" onClick={handleReset} />
+                  <input
+                    type="submit"
+                    value="SUBMIT"
+                    onClick={() => setSubmit(true)}
+                  />
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </fieldset>
+      </form>
     </Layout>
   );
 }
