063be614ef11dd8a0960b8ce3a61df3cf78d2d5b
Bae Sujeong <doya60@gmail.com>
2022-06-12 13:26:28 +0900

    로컬저장소에 초기 데이터 없을 경우 오류 방지 위해 더미데이터 출력

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -12,7 +12,19 @@ function Community() {
 
   const getLocalData = () => {
     const data = localStorage.getItem("post");
-    return JSON.parse(data);
+    const dummyPosts = [
+      { title: "Hello5", content: "Here comes description in detail." },
+      { title: "Hello4", content: "Here comes description in detail." },
+      { title: "Hello3", content: "Here comes description in detail." },
+      { title: "Hello2", content: "Here comes description in detail." },
+      { title: "Hello1", content: "Here comes description in detail." },
+    ];
+
+    if (data) {
+      return JSON.parse(data);
+    } else {
+      return dummyPosts;
+    }
   };
 
   const [Posts, setPosts] = useState(getLocalData());
