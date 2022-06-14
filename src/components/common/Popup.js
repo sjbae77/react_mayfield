455cab27f4d17f34420b5097df7e7a850ebb924f
Bae Sujeong <doya60@gmail.com>
2022-06-14 17:59:12 +0900

    갤러리 작업

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,24 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useState, forwardRef, useImperativeHandle } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
-function Popup({ children, setOpen }) {
+/*
+	단계1 - 기존의 컴포넌트 함수를 대입형(화살표) 함수로 변경
+	단계2 - 해당 화살표함수를 forwardRef()의 인수로 전달
+	단계3 - forwardRef의 인수로 전달받는 함수의 두번채 파라미터로 ref추가
+*/
+
+const Popup = forwardRef(({ children }, ref) => {
+  //자신의 open여부를 결정하는 state생성
+  const [Open, setOpen] = useState(false);
+
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    Open
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [Open]);
 
-    return () => {
-      document.body.style.overflow = "auto";
+  //해당 컴포넌트에서 만들어지는 함수를 부모컴포넌트에서 사용가능하도록 외부로 반환가능
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => setOpen(true), //팝업열기 함수
+      close: () => setOpen(false), //팝업닫기 함수
     };
   });
 
   return (
-    <aside className="pop">
-      <div className="cont">
-        {children}
-        <span className="btnClose" onClick={() => setOpen(false)}>
-          close
-        </span>
-      </div>
-    </aside>
+    <>
+      <AnimatePresence>
+        {Open && (
+          <motion.aside
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              transition: { duration: 0.5 },
+            }}
+            exit={{
+              opacity: 0,
+              x: "-100%",
+              transition: { duration: 0.5, delay: 0.3 },
+            }}
+            className="pop"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { duration: 0.5, delay: 0.5 },
+              }}
+              exit={{ opacity: 0, transition: { duration: 0.3 } }}
+              className="con"
+            >
+              {children}
+              <span className="close" onClick={() => setOpen(false)}>
+                close
+              </span>
+            </motion.div>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+    </>
   );
-}
+});
 
 export default Popup;
--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,24 +1,148 @@
-import React from "react";
 import Layout from "../common/Layout";
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+import Masonry from "react-masonry-component";
+import Popup from "../common/Popup";
 
 function Gallery() {
+  const frame = useRef(null);
+  const pop = useRef(null);
+  const [Items, setItems] = useState([]);
+  const [Loading, setLoading] = useState(true);
+  const [Index, setIndex] = useState(0);
+  const masonryOptions = { transitionDuration: "0.5s" };
+
+  const getFlickr = async (opt) => {
+    const key = "91235d20021c4d6439c8426d95043091";
+    const method_interest = "flickr.interestingness.getList";
+    const method_search = "flickr.photos.search";
+    const method_user = "flickr.people.getPhotos";
+    let url = "";
+
+    if (opt.type === "interest") {
+      url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${opt.count}&nojsoncallback=1&format=json`;
+    }
+    if (opt.type === "search") {
+      url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=${opt.count}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+    }
+    if (opt.type === "user") {
+      url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${key}&per_page=${opt.count}&nojsoncallback=1&format=json&user_id=${opt.user}`;
+    }
+
+    await axios.get(url).then((json) => {
+      //만약 검색 결과가 없다면 경고창 띄우고 종료
+      if (json.data.photos.photo.length === 0)
+        return alert("해당검색어의 결과이미지 없습니다.");
+      setItems(json.data.photos.photo);
+    });
+
+    setTimeout(() => {
+      if (!frame.current) return;
+      frame.current.classList.add("on");
+      setLoading(false);
+    }, 1000); //데이터준비 완료될때까지 지연
+  };
+
+  useEffect(() => {
+    getFlickr({
+      type: "user",
+      count: 20,
+      user: "195772706@N08",
+    });
+  }, []);
+
   return (
-    <Layout name={"Gallery"}>
-      <div className="left">
-        <div className="title-wrap">
-          <h1>ROOM</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-            quo!
-          </p>
+    <>
+      <Layout name={"Gallery"}>
+        <div className="left">
+          <div className="title-wrap">
+            <h1>ROOM</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              quo!
+            </p>
+          </div>
+
+          {Loading && (
+            <img
+              className="loading"
+              src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+            />
+          )}
+
+          <div className="frame" ref={frame}>
+            <Masonry elementType={"div"} options={masonryOptions}>
+              {Items.map((item, idx) => {
+                return (
+                  <article key={idx}>
+                    <div className="inner">
+                      <div
+                        className="pic"
+                        onClick={() => {
+                          pop.current.open();
+                          setIndex(idx);
+                        }}
+                      >
+                        <img
+                          src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+                          alt={item.title}
+                        />
+                      </div>
+                      <div className="info">
+                        <h2>{item.title}</h2>
+                        <div className="profile">
+                          <img
+                            src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+                            alt={item.owner}
+                            onError={(e) => {
+                              //해당 이미지요소의 소스이미지가 없어서 onError이벤트가 발생하면 src값을 대체이미지로 변경
+                              e.target.setAttribute(
+                                "src",
+                                "https://www.flickr.com/images/buddyicon.gif"
+                              );
+                            }}
+                          />
+                          <span
+                            onClick={(e) => {
+                              setLoading(true);
+                              frame.current.classList.remove("on");
+
+                              getFlickr({
+                                type: "user",
+                                count: 50,
+                                user: e.currentTarget.innerText,
+                              });
+                            }}
+                          >
+                            {item.owner}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                );
+              })}
+            </Masonry>
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <div className="title-wrap">
-          <h1>HOTEL</h1>
+
+        <div className="right">
+          <div className="title-wrap">
+            <h1>HOTEL</h1>
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+
+      {/* 컴포넌트자체를 useRef로 참조 */}
+      <Popup ref={pop}>
+        {Items.length !== 0 && (
+          <img
+            src={`https://live.staticflickr.com/${Items[Index].server}/${Items[Index].id}_${Items[Index].secret}_b.jpg`}
+            alt={Items[Index].title}
+          />
+        )}
+      </Popup>
+    </>
   );
 }
 
