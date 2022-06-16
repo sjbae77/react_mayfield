fe6757e0267cdf2e88485abbc9518db9d9f79bf7
Bae Sujeong <doya60@gmail.com>
2022-06-16 20:44:41 +0900

    main - visual 스와이퍼, mousemove 이벤트 적용

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,13 +1,52 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useRef, useEffect } from "react";
+import "swiper/css";
 
 function Visual() {
   const path = process.env.PUBLIC_URL;
+  const cursor = useRef(null);
+  const frame = useRef(null);
+  let isCursor = false;
+
+  const mouseMove = (e) => {
+    if (!isCursor) return;
+    cursor.current.style.left = e.clientX + "px";
+    cursor.current.style.top = e.clientY + "px";
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseMove);
+    frame.current.addEventListener("mouseenter", () => {
+      isCursor = true;
+      cursor.current.style.display = "block";
+    });
+    frame.current.addEventListener("mouseleave", () => {
+      isCursor = false;
+      cursor.current.style.display = "none";
+    });
+  }, []);
 
   return (
     <section id="visual">
       <div className="inner">
         <div className="mainBox">
-          <img src={`${path}/img/main_visual.jpg`} alt="visual image" />
+          <Swiper
+            slidesPerView={1}
+            loop={true}
+            centeredSlides={true}
+            ref={frame}
+          >
+            <SwiperSlide>
+              <img src={`${path}/img/main_visual1.jpg`} alt="visual image" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={`${path}/img/main_visual2.jpg`} alt="visual image" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={`${path}/img/main_visual3.jpg`} alt="visual image" />
+            </SwiperSlide>
+          </Swiper>
+          {/* <img src={`${path}/img/main_visual1.jpg`} alt="visual image" /> */}
           <div className="title-wrap">
             <h1>
               We are making
@@ -31,6 +70,10 @@ function Visual() {
             adipisci, sint laboriosam officia
           </p>
         </div>
+      </div>
+
+      <div className="cursor" ref={cursor}>
+        drag
       </div>
     </section>
   );
