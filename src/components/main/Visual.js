3185405ad56ae5ffbcd71c941ef265afb9e5b717
Bae Sujeong <doya60@gmail.com>
2022-06-16 20:08:32 +0900

    메인 - visual web작업

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Visual() {
-  return <section id="visual">Visual</section>;
+  const path = process.env.PUBLIC_URL;
+
+  return (
+    <section id="visual">
+      <div className="inner">
+        <div className="mainBox">
+          <img src={`${path}/img/main_visual.jpg`} alt="visual image" />
+          <div className="title-wrap">
+            <h1>
+              We are making
+              <br />
+              real life.
+            </h1>
+            <span className="shape_paral">
+              <a href="#">read more about us</a>
+            </span>
+          </div>
+        </div>
+
+        {/* <div className="pic">
+          <img src={`${path}/img/main_visual2.jpg`} alt="visual image" />
+        </div> */}
+        <div className="textBox">
+          <em>LAST VIDEO</em>
+          <span>Lorem, ipsum dolor sit amet.</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            adipisci, sint laboriosam officia
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Visual;
