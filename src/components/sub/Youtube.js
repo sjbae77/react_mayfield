0c502c6725668eacab685a94a807fb26036d990c
Bae Sujeong <doya60@gmail.com>
2022-06-17 18:17:25 +0900

    유튜브 오류 수정

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -47,9 +47,9 @@ function Youtube() {
                 height="315"
                 src="https://www.youtube.com/embed/JZxOBtrnLxw"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
+                allowFullScreen
               ></iframe>
               <span>
                 MAIN
