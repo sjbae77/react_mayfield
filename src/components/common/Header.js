1e3a0788769ea6336da2477f5504540507a4f14f
Bae Sujeong <doya60@gmail.com>
2022-06-21 16:55:04 +0900

    네비 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,52 +1,63 @@
 import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+import Menu from "./Menu";
 
 function Header({ type }) {
+  const menu = useRef(null);
   const style = { color: "darkgreen", fontWeight: "600" };
+
   return (
-    <header className={type}>
-      <div className="inner">
-        <h1>
-          <Link to="/">Mayfield</Link>
-        </h1>
+    <>
+      <header className={type}>
+        <div className="inner">
+          <h1>
+            <Link to="/">Mayfield</Link>
+          </h1>
 
-        <ul id="gnb">
-          <li>
-            <NavLink activeStyle={style} to="/about">
-              ABOUT
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={style} to="/gallery">
-              GALLERY
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={style} to="/youtube">
-              YOUTUBE
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={style} to="/community">
-              COMMUNITY
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={style} to="/location">
-              LOCATION
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeStyle={style} to="/join">
-              JOIN
-            </NavLink>
-          </li>
-        </ul>
+          <ul id="gnb">
+            <li>
+              <NavLink activeStyle={style} to="/about">
+                ABOUT
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/gallery">
+                GALLERY
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/youtube">
+                YOUTUBE
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/community">
+                COMMUNITY
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/location">
+                LOCATION
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/join">
+                JOIN
+              </NavLink>
+            </li>
+          </ul>
 
-        <FontAwesomeIcon icon={faBars} />
-      </div>
-    </header>
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={() => menu.current.toggle()}
+          />
+        </div>
+      </header>
+
+      <Menu ref={menu} />
+    </>
   );
 }
 
--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,7 +1,73 @@
-import React from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { NavLink } from "react-router-dom";
 
-function Menu() {
-  return <div>Menu</div>;
-}
+const Menu = forwardRef((props, ref) => {
+  const [Open, setOpen] = useState(false);
+  const style = { color: "darkgreen" };
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggle: () => setOpen(!Open),
+    };
+  });
+
+  return (
+    <AnimatePresence>
+      {Open && (
+        <motion.nav
+          id="gnbMb"
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 0.5 },
+          }}
+          exit={{ y: "-100%", opacity: 0 }}
+          onClick={() => setOpen(!Open)}
+        >
+          <h1>
+            <NavLink exact to="/">
+              Mayfield
+            </NavLink>
+          </h1>
+
+          <ul>
+            <li>
+              <NavLink activeStyle={style} to="/about">
+                ABOUT
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/gallery">
+                GALLERY
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/youtube">
+                YOUTUBE
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/community">
+                COMMUNITY
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/location">
+                LOCATION
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeStyle={style} to="/join">
+                JOIN
+              </NavLink>
+            </li>
+          </ul>
+        </motion.nav>
+      )}
+    </AnimatePresence>
+  );
+});
 
 export default Menu;
